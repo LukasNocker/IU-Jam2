99ea62a7fd63f2b86e7863a1d1680fca4e0943aa
Lukas Nocker <lukasnocker@gmail.com>
2022-03-26 03:40:48 +0100

    Test Scene for Tiles, added Test Tile Set from Asset Store, Added basic Charakter Movement

---
+++ IU-Jam2/Assets/Luky Workbanch/Skript/CharakterController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharakterController : MonoBehaviour
+{
+    private Rigidbody2D rb;
+   
+    // movement variables
+    private float moveH;
+    private float moveV;
+    [SerializeField] private float moveSpeed = 1.0f;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+    }
+
+    private void FixedUpdate()
+    {
+        moveH = Input.GetAxis("Horizontal") * moveSpeed;
+
+        moveV = Input.GetAxis("Vertical") * moveSpeed;
+
+        rb.velocity = new Vector2(moveH, moveV);
+
+    }
+}
