f54e942eb3df9210ecc8d4bda98fc6f5b879805c
GodTGamer <66565374+GodTGamer@users.noreply.github.com>
2022-03-26 19:45:30 +0100

    Hide and Show
    Credits and Controls haben nun ein hide and show

--- IU-Jam2/Assets/Dominic Workbanch/Scripts/MenuSkript.cs
+++ IU-Jam2/Assets/Dominic Workbanch/Scripts/MenuSkript.cs
@@ -6,6 +6,26 @@ using UnityEngine.UI;
 
 public class MenuSkript : MonoBehaviour
 {
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!PlayerPrefs.HasKey("musicVolume"))
+        {
+            PlayerPrefs.SetFloat("musicVolume", 1);
+            Load();
+        }
+        else
+        {
+            Load();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
     [SerializeField] Slider volumeSlider;
 
     public void PlayGame()
@@ -13,14 +33,24 @@ public class MenuSkript : MonoBehaviour
 
     }
 
-    public void ShowCredits()
+    public void ShowCredits(GameObject obj)
     {
-
+        obj.SetActive(true);
     }
 
-    public void ShowControls()
+    public void HideCredits(GameObject obj)
     {
+        obj.SetActive(false);
+    }
 
+    public void ShowControls(GameObject obj)
+    {
+        obj.SetActive(true);
+    }
+
+    public void HideControls(GameObject obj)
+    {
+        obj.SetActive(false);
     }
 
     public void QuitGame()
@@ -48,23 +78,5 @@ public class MenuSkript : MonoBehaviour
     {
         PlayerPrefs.SetFloat("musicVolume", volumeSlider.value);
     }
-    // Start is called before the first frame update
-    void Start()
-    {
-        if (!PlayerPrefs.HasKey("musicVolume"))
-        {
-            PlayerPrefs.SetFloat("musicVolume", 1);
-            Load();
-        }
-        else
-        {
-            Load();
-        }
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
 }
