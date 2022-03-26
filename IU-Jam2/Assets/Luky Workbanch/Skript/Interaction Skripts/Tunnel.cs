dc034bb3b6caec1e6eddd206e74683e9e6e488d8
Lukas Nocker <lukasnocker@gmail.com>
2022-03-27 00:36:03 +0100

    Charakter Animations added

--- IU-Jam2/Assets/Luky Workbanch/Skript/Interaction Skripts/Tunnel.cs
+++ IU-Jam2/Assets/Luky Workbanch/Skript/Interaction Skripts/Tunnel.cs
@@ -4,15 +4,44 @@ using UnityEngine;
 
 public class Tunnel : MonoBehaviour
 {
+    private bool search;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        search = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (search == true)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                Debug.Log("oh i found a cookie");
+
+                Debug.Log("oh i flound one of my babys");
+                //Spawn game Object: Cookie
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            Debug.Log("Interact E");
+
+            // TextBox: Vöt. kann ich da ja ein Baby durchschicken
+            search = true;
+
+
+        }
+
+        else
+        {
+            search = false;
+        }
     }
 }
