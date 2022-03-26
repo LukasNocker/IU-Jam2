70374413ddff123379a59fef12c54f37482942ce
GodTGamer <66565374+GodTGamer@users.noreply.github.com>
2022-03-26 17:29:06 +0100

    MainMenu
    MainMenu ist drin

---
+++ IU-Jam2/Assets/Dominic Workbanch/Scripts/MenuSkript.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuSkript : MonoBehaviour
+{
+    public void PlayGame()
+    {
+
+    }
+
+    public void ShowCredits()
+    {
+
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void BackToMenu()
+    {
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
