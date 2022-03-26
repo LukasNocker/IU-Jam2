cca5d05f1dc6001d2c790671be50d5e7fb89654c
GodTGamer <66565374+GodTGamer@users.noreply.github.com>
2022-03-26 18:24:30 +0100

    VolumeSlider
    Der Volume Slider sollte nun funktionieren und im Script sind Platzhalter fuer die Buttons

--- IU-Jam2/Assets/Dominic Workbanch/Scripts/MenuSkript.cs
+++ IU-Jam2/Assets/Dominic Workbanch/Scripts/MenuSkript.cs
@@ -2,9 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MenuSkript : MonoBehaviour
 {
+    [SerializeField] Slider volumeSlider;
+
     public void PlayGame()
     {
 
@@ -24,10 +27,34 @@ public class MenuSkript : MonoBehaviour
     {
 
     }
+
+    public void ChangeVolume()
+    {
+        AudioListener.volume = volumeSlider.value;
+        Save();
+    }
+
+    private void Load()
+    {
+        volumeSlider.value = PlayerPrefs.GetFloat("musicVolume");
+    }
+
+    private void Save()
+    {
+        PlayerPrefs.SetFloat("musicVolume", volumeSlider.value);
+    }
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (!PlayerPrefs.HasKey("musicVolume"))
+        {
+            PlayerPrefs.SetFloat("musicVolume", 1);
+            Load();
+        }
+        else
+        {
+            Load();
+        }
     }
 
     // Update is called once per frame
